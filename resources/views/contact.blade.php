5341442f271862884ee2ae752c735f573d0a0c45
Andrey Kasatkin <a@svetliy.com>
2016-12-15 13:38:14 -0500

    add ids

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,7 +19,7 @@
 
     {!! Form::open(array('route' => 'contact_store', 'class' => 'form')) !!}
 
-    <div class="form-group">
+    <div class="form-group" id="name">
         {!! Form::label('Your Name') !!}
         {!! Form::text('name', null,
             array('required',
